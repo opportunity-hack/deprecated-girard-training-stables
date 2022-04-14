f1bb7c4edf09026e914578891be4a9ff88b6a088
ShreyasNarayanan18 <snaray52@asu.edu>
2022-04-14 15:02:08 -0700

    Added UPDATE, DELETE, and GET functionality to the lesson controller, still need to figure out emailing

--- server/src/controllers/lessons.js
+++ server/src/controllers/lessons.js
@@ -2,13 +2,43 @@ const Lesson = require("../models/lessons");
 const { getFormattedDateTime, convertTimeFormat } = require("../utils/helper");
 const { dataHandler } = require("../utils/responseHandler");
 const { sendLessonMail } = require("./../utils/emailer");
+const asyncHandler = require('express-async-handler');
 
 module.exports.createLesson = asyncHandler(async function(req, res) {
-    const response = await Lesson.create(req.body);
+    const response = await Lesson.insertMany(req.body);
     res.status(200).json(response);
     
-    sendLessonMail({
+    /*sendLessonMail({
       userEmail: 'developer.akash.s@gmail.com',
       name: 'Amy'
-    });
+    });*/
+});
+
+module.exports.getLesson = asyncHandler(async function(req, res) {
+  const response = await Lesson.find({});
+  res.status(200).json(response);
+});
+
+module.exports.updateLesson = asyncHandler(async function(req, res) {
+  const id = await Lesson.findById(req.params.id)
+
+  if (!id) {
+    res.status(400);
+    throw new Error("Lesson not found")
+  }
+
+  const response = await Lesson.findByIdAndUpdate(req.params.id, req.body, {new: true});
+  res.status(200).json(response);
+});
+
+module.exports.deleteLesson = asyncHandler(async function(req, res) {
+  const id = await Lesson.findById(req.params.id);
+
+  if (!id) {
+      res.status(400);
+      throw new Error("Lesson not found");
+  }
+  
+  await id.delete();
+  res.status(200).json({ id: req.params.id });
 });
