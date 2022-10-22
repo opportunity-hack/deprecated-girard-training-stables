31ee8249b259b55ca7b9a91b28a262edb20cef88
Aaron Yu <aaronyu.main@gmail.com>
2022-10-21 18:55:09 -0700

    added auth0 jwt code, needs testing on heroku

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const connectDB = require('./src/models/db/connector');
 const { errorHandler } = require('./src/middlewares/errorHandler');
 // body-parser no longer needed in newer versions of express -aaron
 // const bodyParser = require ('body-parser');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+
+
 
 // environment variables in .env file
 require('dotenv').config();
@@ -25,6 +29,18 @@ app.use(express.json());
 const horseRouter = require('./src/routes/horse');
 const lessonRouter = require('./src/routes/lesson');
 const userRouter = require('./src/routes/user');
+
+app.use(jwt({
+    secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 25,
+    jwksUri: 'https://dev-6ir-6qcd.us.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'https://girard-server.herokuapp.com/',
+    issuer: 'https://dev-6ir-6qcd.us.auth0.com/',
+    algorithms: ['RS256']
+}));
 
 app.use('/horses', horseRouter);
 app.use('/lessons', lessonRouter);
