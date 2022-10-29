d095a3fd7ce9006de8eb0910d219127b1ff094a9
aaronyu888 <aaronyu.main@gmail.com>
2022-10-28 22:17:32 -0700

    protect express app using auth0

--- server/index.js
+++ server/index.js
@@ -5,10 +5,9 @@ const connectDB = require('./src/models/db/connector');
 const { errorHandler } = require('./src/middlewares/errorHandler');
 // body-parser no longer needed in newer versions of express -aaron
 // const bodyParser = require ('body-parser');
-const jwt = require('express-jwt');
+const { expressjwt: jwt } = require('express-jwt');
 const jwks = require('jwks-rsa');
-
-
+const { auth } = require('express-oauth2-jwt-bearer');
 
 // environment variables in .env file
 require('dotenv').config();
@@ -22,25 +21,26 @@ const app = express();
 // port that server will run on
 const port = process.env.PORT || 5000;
 
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-6ir-6qcd.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'https://test-api/',
+  issuer: 'https://dev-6ir-6qcd.us.auth0.com/',
+  algorithms: ['RS256']
+});
+
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(jwtCheck);
 
 const horseRouter = require('./src/routes/horse');
 const lessonRouter = require('./src/routes/lesson');
 const userRouter = require('./src/routes/user');
-
-app.use(jwt({
-    secret: jwks.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 25,
-    jwksUri: 'https://dev-6ir-6qcd.us.auth0.com/.well-known/jwks.json'
-    }),
-    audience: 'https://girard-server.herokuapp.com/',
-    issuer: 'https://dev-6ir-6qcd.us.auth0.com/',
-    algorithms: ['RS256']
-}));
 
 app.use('/horses', horseRouter);
 app.use('/lessons', lessonRouter);
