0f49fe2ea19d55f26ded6a92dd9344ffc5e2d3aa
ShreyasNarayanan18 <snaray52@asu.edu>
2022-11-20 10:27:32 -0800

    Added check in accordion if there are events with no volunteers

--- client/src/components/Accordion/Accordion.js
+++ client/src/components/Accordion/Accordion.js
@@ -67,46 +67,37 @@ const [subExpanded, setSubExpanded] = React.useState('');
   let data = props.data;
 
   const handleSignUpForEvent = (position) => {
-    //TODO: SIGN UP FLOW
-    //window.alert(position);
+    //TODO: SIGN UP FLow
+    //TEST MESSGE
     console.log(position);
-  }
-
-
-  //toLocaleString('en-GB', { timeZone: 'UTC' })
-
-  const getDate = (dateString) => {
-    console.log("startDate:", dateString);
-    let startDate = new Date(dateString);
-    return startDate.toLocaleString('en-US', {timeZone: 'MST'});
-    //return startDate.toString();
   }
 
   return (
         <div>
             <CloseIcon style={{float: 'right', fontSize: '2rem', cursor: 'pointer'}} onClick={props.handleClose} />
-            <div className="modal-heading">{data.title} - {getDate(data.start)}</div>
+            <div className="modal-heading">{data.title}- {data.start.toDateString()}</div>
             {
               data && Object.keys(data.volunteers).map((pos, index) => {
                 return (
-                  <div key={v4()}>
-                      <Accordion key={v4()} square expanded={expanded === `panel-${index}`} onChange={handleChange(`panel-${index}`)}>
-                          <AccordionSummary aria-controls="panel1d-content" id={`panel1d-header-${index}`}>
-                            <Typography >{pos}</Typography>
-                          </AccordionSummary>
-                          <AccordionDetails >
-                              <Accordion key={v4()} square expanded={subExpanded === `subpanel-${pos}-${index}`} onChange={handleSubChange(`subpanel-${pos}-${index}`)}>
-                                  <AccordionSummary id={`subpanel-${index}-header`} >
-                                    <div style={{ display: 'flex', flex: '1 1 auto', justifyContent: 'space-between', alignItems: 'center', verticalAlign:'center', height: '1rem'}}>
-                                        <div>{data.volunteers[pos].signedUp.length} of {data.volunteers[pos].minVolunteers} filled</div>
-                                        {data.volunteers[pos].minVolunteers - data.volunteers[pos].signedUp.length !== 0 ? <Button variant="contained" color="primary" onClick={() => handleSignUpForEvent(pos)}>Sign up</Button> : <></>}
-                                        {/* <Button key={v4()} variant="contained" color="primary" onClick={handleSignUpForEvent(pos)}>Sign up</Button> */}
-                                    </div>
-                                  </AccordionSummary>
-                              </Accordion>
-                          </AccordionDetails>
-                      </Accordion>
-                  </div>
+                    <div key={v4()}>
+                      {data.volunteers[pos].minVolunteers >= 1 ?
+                        <Accordion key={v4()} square expanded={expanded === `panel-${index}`} onChange={handleChange(`panel-${index}`)}>
+                            <AccordionSummary aria-controls="panel1d-content" id={`panel1d-header-${index}`}>
+                              <Typography >{pos}</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails >
+                                <Accordion key={v4()} square expanded={subExpanded === `subpanel-${pos}-${index}`} onChange={handleSubChange(`subpanel-${pos}-${index}`)}>
+                                    <AccordionSummary id={`subpanel-${index}-header`} >
+                                      <div style={{ display: 'flex', flex: '1 1 auto', justifyContent: 'space-between', alignItems: 'center', verticalAlign:'center', height: '1rem'}}>
+                                          <div>{data.volunteers[pos].signedUp.length} of {data.volunteers[pos].minVolunteers} filled</div>
+                                          {data.volunteers[pos].minVolunteers - data.volunteers[pos].signedUp.length !== 0 ? <Button variant="contained" color="primary" onClick={() => handleSignUpForEvent(pos)}>Sign up</Button> : <></>}
+                                      </div>
+                                    </AccordionSummary>
+                                </Accordion>
+                            </AccordionDetails>
+                        </Accordion>
+                      : <></>}
+                    </div>
                 )
               })
             }
