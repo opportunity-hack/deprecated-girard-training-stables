ab4a69e0d8eabe3e984991d8ac858c135bf3326d
Benjamin Ang <ben.ang@fastmail.com>
2023-05-12 09:44:56 -0400

    Change redirectUri to redirect_uri
    This silences a warning from the auth0 SDK about how `redirectUri` is
    deprecated and will be unsupported in future versions in favor of
    `redirect_uri`.

--- client/src/index.js
+++ client/src/index.js
@@ -24,7 +24,7 @@ root.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-    redirectUri={window.location.origin + "/signup"}
+    redirect_uri={window.location.origin + "/signup"}
   >
     <React.StrictMode>
       <BrowserRouter>
