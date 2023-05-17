2703f72e8bec245249e895f2207fdf36506b651e
Benjamin Ang <ben.ang@fastmail.com>
2023-05-16 21:04:17 -0400

    Make fields on User model optional
    In the current implementation, a user can sign up and get access to the
    frontend application without providing any information other than an
    email for Auth0 signup. I'm changing firstName and phoneNumber so they
    aren't required, and adding a default value for userType. This way, a
    User entity in the backend database can be created as soon as Auth0
    registration is completed, with remaining information to be filled in
    afterwards.

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: true
+    required: false
   },
   lastName: {
     type: String,
@@ -13,7 +13,8 @@ const userSchema = new mongoose.Schema({
   userType: {
     type: String,
     enum: ['instructor', 'volunteer', 'volunteer coordinator'],
-    required: true
+    required: true,
+    default: "volunteer"
   },
   email: {
     type: String,
@@ -24,7 +25,7 @@ const userSchema = new mongoose.Schema({
   },
   phoneNumber: {
     type: String,
-    required: true,
+    required: false,
     match: [/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/, "Please provide a valid phone number"]
   },
   // in inches
