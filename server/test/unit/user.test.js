27f22158bf3ed9bf9f4b174b528e75e8d8d05469
Benjamin Ang <ben.ang@fastmail.com>
2023-05-21 16:46:35 -0400

    Use generated instead of hardcoded values in test
    Added typescript, faker, and test-data-bot to the project. Modified the
    tests to ensure that their functioning is not based on particular
    hardcoded values.

--- server/test/unit/user.test.js
+++ server/test/unit/user.test.js
@@ -1,10 +1,14 @@
 import db from "../utils/database";
+import { memoizeUnique } from "../utils/memoizeUnique";
 import request from "supertest";
 import express from "express";
 import User from "../../src/models/users";
 import { createUser,
         getUsers,
 } from "../../src/controllers/user";
+import { faker } from "@faker-js/faker";
+import { build, perBuild, oneOf }  from '@jackfranklin/test-data-bot';
+
 
 beforeAll(async () => {
     await db.connect();
@@ -22,35 +26,45 @@ const app = express();
 
 // Mock Data
 
-const validUser = {
-    user_id: "auth0|507f1f77bcf86cd799439020",
-    firstName: "joe",
-    lastName: "schmoe",
-    userType: "volunteer",
-    email: "joe.schmoe@notreal.com",
-    phoneNumber: "123-456-7890",
-    horseExperience: 1000,
-    horseRiding: false,
-    horseTacking: false,
-    horseGrooming: false,
-    horseLeading: false,
-}
+const userTypes = ["instructor", "volunteer", "volunteer coordinator"]
+
+const newUniqueAuth0ID = memoizeUnique(() => `auth0|${faker.string.alphanumeric(24)}`)
+
+const newUniqueEmail = memoizeUnique(faker.internet.email)
+
+const newValidUserBuilder = build({
+    fields: {
+    user_id: perBuild(newUniqueAuth0ID),
+    firstName: perBuild(faker.person.firstName),
+    lastName: perBuild(faker.person.lastName),
+    userType: oneOf(...userTypes),
+    email: perBuild(newUniqueEmail),
+    phoneNumber: perBuild(() => faker.phone.number('###-###-####')),
+    horseExperience: perBuild(faker.number.int),
+    horseRiding: perBuild(faker.datatype.boolean),
+    horseTacking: perBuild(faker.datatype.boolean),
+    horseGrooming: perBuild(faker.datatype.boolean),
+    horseLeading: perBuild(faker.datatype.boolean),
+    }
+})
+
+const validUser = newValidUserBuilder.one()
 
 const validUserIncomplete = {
-    user_id: "auth0|123456",
-    email: "alice@example.com"
+    user_id: newUniqueAuth0ID(),
+    email: newUniqueEmail(),
 }
 
 const inValidUserNoEmail = {
-    firstName: "joe",
-    lastName: "schmoe",
-    userType: "volunteer",
-    phoneNumber: "123-456-7890",
-    horseExperience: 1000,
-    horseRiding: false,
-    horseTacking: false,
-    horseGrooming: false,
-    horseLeading: false,
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    userType: faker.helpers.arrayElement(userTypes),
+    phoneNumber: faker.phone.number('###-###-####'),
+    horseExperience: faker.number.int(),
+    horseRiding: faker.datatype.boolean(),
+    horseTacking: faker.datatype.boolean(),
+    horseGrooming: faker.datatype.boolean(),
+    horseLeading: faker.datatype.boolean(),
 }
 
 // Route setup
