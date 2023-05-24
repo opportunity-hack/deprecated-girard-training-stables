b3f7b60f6b87e60c16fe9f010996c3d970a9decf
Benjamin Ang <ben.ang@fastmail.com>
2023-05-24 11:56:36 -0400

    Implement admin UI using MUI data-grid
    Displays information from all relevant fields of the server database as
    well as additional information from the Auth0 management API in a table.
    The MUI data-grid comes with many built-in features such as sorting and
    filtering. Where applicable, fields can be double-clicked to toggle edit
    mode, but for now this doesn't propogate any changes anywhere. This is a
    read-only view.

--- client/src/components/AdminUI/AdminUI.js
+++ client/src/components/AdminUI/AdminUI.js
@@ -1,20 +1,109 @@
-import React from "react";
 import { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Checkbox, Container, Typography, Button, List, ListItem, ListItemText, TextField } from '@mui/material';
+import { Container, Typography, Button } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 import httpClient from '../../httpClient';
 
 function AdminUI() {
+  const [isAdmin, setIsAdmin] = useState(false)
   const { isAuthenticated, user, loginWithRedirect } = useAuth0();
-  const isAdmin = user?.["https://girard-server.herokuapp.com/roles"]?.includes('admin');
   const [users, setUsers] = useState([]);
   useEffect(() => {
-      if (isAuthenticated && isAdmin) {
-          httpClient.get(process.env.REACT_APP_API_SERVER + "/users")
-              .then(res => setUsers(res.data))
-      }
-  }, [isAuthenticated, isAdmin])
-    if (!(isAuthenticated) || !(isAdmin)) {
+        setIsAdmin(user?.["https://girard-server.herokuapp.com/roles"]?.includes('admin'));
+  })
+  useEffect(() => {
+      httpClient.get(process.env.REACT_APP_API_SERVER + "/users")
+          .then(res => setUsers(res.data))
+  }, []);
+
+    const columns = [
+        {
+          field: 'email',
+          headerName: 'Email',
+          editable: 'true',
+          width: '190',
+        },
+        {
+          field: 'phoneNumber',
+          headerName: 'Phone ',
+          editable: 'true',
+          width: '190',
+        },
+        { field: 'firstName',
+          headerName: 'First name',
+          editable: true,
+        },
+        { field: 'lastName',
+          headerName: 'Last name',
+          editable: true,
+        },
+        {
+          field: 'isAdmin',
+          headerName: 'Admin',
+          editable: true,
+          type: 'boolean',
+        },
+        {
+          field: 'last_login',
+          headerName: 'Last login',
+          editable: false,
+          width: '100',
+          valueFormatter: (params) => {
+              const date = new Date(params?.value)
+              return date.toLocaleDateString("en-US")
+          },
+        },
+        {
+            field: 'height',
+            headerName: 'height',
+            type: 'number',
+            editable: true,
+            width: '100',
+        },
+        {
+            field: 'age',
+            headerName: 'age',
+            type: 'number',
+            editable: true,
+            width: '100',
+        },
+        {
+            field: 'horseExperience',
+            headerName: 'Experience',
+            type: 'number',
+            editable: true,
+            width: '150',
+            valueFormatter: (params) => `${params?.value} years`,
+        },
+        {
+          field: 'horseRiding',
+          headerName: 'Riding',
+          editable: true,
+          type: 'boolean',
+        },
+        {
+          field: 'horseTacking',
+          headerName: 'Tacking',
+          editable: true,
+          type: 'boolean',
+        },
+        {
+          field: 'horseGrooming',
+          headerName: 'Grooming',
+          editable: true,
+          type: 'boolean',
+        },
+        {
+          field: 'horseLeading',
+          headerName: 'Leading',
+          editable: true,
+          type: 'boolean',
+        },
+    ]
+
+    let rows = users;
+
+    if (!isAuthenticated) {
         return (
           <Container sx={{ marginTop: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Typography variant="h5" gutterBottom>
@@ -25,8 +114,26 @@ function AdminUI() {
         );
     }
 
-    return (
-        <div>{ JSON.stringify(users) }</div>
+    if  (!isAdmin) {
+        return (
+          <Container sx={{ marginTop: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Typography variant="h5" gutterBottom>
+              You are not authorized to view this page.
+            </Typography>
+          </Container>
+        );
+    }
+
+    return isAuthenticated && isAdmin && (
+          <Container sx={{ marginTop: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <DataGrid
+                getRowId={(user) => user.user_id}
+                rows={rows}
+                columns={columns}
+                checkboxSelection
+                disableRowSelectionOnClick
+            />
+          </Container>
     )
 }
 
