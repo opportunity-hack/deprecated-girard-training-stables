0b674c2ef0f7f386a8f8bd302ad26fa51f6cef74
Benjamin Ang <ben.ang@fastmail.com>
2023-05-27 08:50:39 -0400

    Extend Auth0 mock to handle user update and delete
    Also corrects the the mocked endpoint for adding users to the admin
    role.

--- server/test/mocks/Auth0handlers.ts
+++ server/test/mocks/Auth0handlers.ts
@@ -15,7 +15,7 @@ interface userMap {
   [key: string]: Auth0User
 }
 
-var userStore: userMap = {}
+export var userStore: userMap = {}
 
 const ApiUrl = `https://${process.env.AUTH0_DOMAIN}/api/v2`
 const usersEndpoint = `${ApiUrl}/users`;
@@ -24,6 +24,8 @@ const adminUsersEndpoint = `${ApiUrl}/roles/${process.env.AUTH0_ADMIN_ROLE_ID}/u
 export const handlers = [
   rest.get(usersEndpoint, mockGetUsers),
   rest.post(usersEndpoint, mockPostUsers),
+  rest.put(usersEndpoint + '/:user_id', mockUpdateUser),
+  rest.delete(usersEndpoint + '/:user_id', mockDeleteUser),
   rest.get(adminUsersEndpoint, mockGetAdmins),
   rest.post(adminUsersEndpoint, mockSetAdmin),
 ]
@@ -52,6 +54,44 @@ function mockGetUsers(req, res, ctx) {
   )
 }
 
+function mockUpdateUser(req, res, ctx) {
+  const { user_id } = req.params;
+  if (!(user_id in userStore)) {
+    return res(
+      ctx.status(404),
+      ctx.json({
+          "statusCode": 404,
+          "error": "Not Found",
+          "message": "The user does not exist.",
+          "errorCode": "inexistent_user"
+      })
+    )
+  }
+
+  // email is the only thing about the Auth0 user entity
+  // that we ever need to modify for our usecase so this
+  // is the only thing we mock out
+  userStore[user_id].email = req.body.email
+  return res(
+    ctx.status(200),
+    ctx.json(userStore[user_id]),
+  )
+}
+
+function mockDeleteUser(req, res, ctx) {
+  const { user_id } = req.params;
+
+  // as long as the user_id has the right format, the real
+  // Auth0 API doesn't actually care if the user to be deleted
+  // exists or not
+  delete userStore[user_id]
+  return res(
+    ctx.set('cache-control', 'no-cache'),
+    ctx.set('content-type', 'application/json; charset=utf-8'),
+    ctx.status(204),
+  )
+}
+
 function mockGetAdmins(req, res, ctx) {
   const adminUsers = Object.values(userStore).filter(user => user.isAdmin)
   return res(
@@ -61,8 +101,8 @@ function mockGetAdmins(req, res, ctx) {
 }
 
 function mockSetAdmin(req, res, ctx) {
-  const { user_id } = req.params
-  userStore[user_id].isAdmin = true
+  const user_ids = req.params
+  user_ids.forEach(user_id => {userStore[user_id].isAdmin = true})
   return res(
     ctx.status(200),
     ctx.json({message:"success"})
